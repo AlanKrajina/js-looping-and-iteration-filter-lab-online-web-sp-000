9949f1d9774f6a069172d85130e99a0832454a02
Alan Krajina <alankrajina88@gmail.com>
2019-09-15 09:59:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
-function findMatching(arr, criteria) {
+function findMatching(arr.charAt(0).toUpperCase(), criteria.charAt(0).toUpperCase()) {
   return arr.filter(function(obj) {
     return Object.keys(criteria).every(function(c) {
       return obj[c] === criteria[c];
@@ -12,3 +12,5 @@ function findMatching(arr, criteria) {
 function fuzzyMatch(){
 
 }
+
+.charAt(0).toUpperCase()
