94b14e2edad384818248155b8152123ed2f5f2e4
Alan Krajina <alankrajina88@gmail.com>
2019-09-15 10:06:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solution in this file
 function findMatching(arr, criteria1) {
-  let criteria = criteria1.charAt(0).toLowerCase() + criteria1.substring(1);
+  let criteria2 = criteria1.charAt(0).toLowerCase() + criteria1.substring(1);
 
   return arr.filter(function(obj) {
-    return Object.keys(criteria).every(function(c) {
+    return Object.keys(criteria2).every(function(c) {
       return obj[c] === criteria[c];
     });
   });
