525c176e88d9d7b49b2d0a6f2b7891119a73c9ec
Alan Krajina <alankrajina88@gmail.com>
2019-09-15 09:47:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Code your solution in this file
-function findMatching(drivers,driver){
-  drivers.filter(function (driver) { return driver === driver; });
+
+function findMatching(arr, criteria) {
+  return arr.filter(function(obj) {
+    return Object.keys(criteria).every(function(c) {
+      return obj[c] == criteria[c];
+    });
+  });
 }
