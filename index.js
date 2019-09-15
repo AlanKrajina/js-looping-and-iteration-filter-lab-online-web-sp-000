2d08fedb32fdc4a08c30830c7bcf5367384a323f
Alan Krajina <alankrajina88@gmail.com>
2019-09-15 10:00:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function findMatching(arr, criteria) {
   return arr.filter(function(obj) {
     return Object.keys(criteria).every(function(c) {
-      return obj[c.charAt(0).toUpperCase()] === criteria[c.charAt(0).toUpperCase()];
+      return obj[c] === criteria[c];
     });
   });
 }
