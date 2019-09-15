a6991c3324d170cd74ba2d1b3957df8c9ac42575
Alan Krajina <alankrajina88@gmail.com>
2019-09-15 10:01:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching(arr1, criteria1) {
-  arr = arr1.replace(/^\w/, c => c.toUpperCase());
-  criteria = criteria1.replace(/^\w/, c => c.toUpperCase());
+  let arr = arr1.replace(/^\w/, c => c.toUpperCase());
+  let criteria = criteria1.replace(/^\w/, c => c.toUpperCase());
 
   return arr.filter(function(obj) {
     return Object.keys(criteria).every(function(c) {
