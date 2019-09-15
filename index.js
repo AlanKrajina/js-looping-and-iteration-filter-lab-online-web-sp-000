96362cd415748732061626f0825d4690b64bd969
Alan Krajina <alankrajina88@gmail.com>
2019-09-15 10:31:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,15 +14,8 @@ function fuzzyMatch(arr, criteria){
 }
 
 
-function matchName(target, criteria){
-  function deepIterator (target) {
-  if (typeof target === 'object') {
-    for (const key in target) {
-      deepIterator(target[key]);
-
-      return target.filter(function(name) {
-        return name === criteria;
-      });
-    }
-  }
+function matchName(arr, criteria){
+  return arr.filter(function(name) {
+    return name.name === criteria.name;
+  });
 }
