cb259e50a5b1355333c50579f9b9b90ee240ebfb
Alan Krajina <alankrajina88@gmail.com>
2019-09-15 10:10:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 function findMatching(arr, criteria) {
   return arr.filter(function(obj) {
     return Object.keys(criteria).every(function(c) {
+      debugger
       return obj[c] === criteria[c];
+
     });
   });
 }
