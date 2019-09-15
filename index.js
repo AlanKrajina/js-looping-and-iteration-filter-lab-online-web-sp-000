d8dae6a0c3a8b649557b298bce2c42686b981c13
Alan Krajina <alankrajina88@gmail.com>
2019-09-15 10:04:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(arr, criteria1) {
-  let criteria = criteria1.charAt(0).toUpperCase() + criteria1.substring(1);
+  let criteria = criteria1.charAt(0).toLowerCase() + criteria1.substring(1);
 
   return arr.filter(function(obj) {
     return Object.keys(criteria).every(function(c) {
