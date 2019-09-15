902f003d0f478f9f9140c6694cb60da483010e8d
Alan Krajina <alankrajina88@gmail.com>
2019-09-15 09:59:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file
-function findMatching(arr.charAt(0).toUpperCase(), criteria.charAt(0).toUpperCase()) {
+function findMatching(arr, criteria) {
   return arr.filter(function(obj) {
     return Object.keys(criteria).every(function(c) {
-      return obj[c] === criteria[c];
+      return obj[c.charAt(0).toUpperCase()] === criteria[c.charAt(0).toUpperCase()];
     });
   });
 }
