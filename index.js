6f09c33573aeacfd82460d4cdade8cc72e458e0d
Alan Krajina <alankrajina88@gmail.com>
2019-09-15 10:18:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file
-function findMatching(arr, criteria) {
+function findMatching(arr, criteria1) {
+  let criteria = criteria1.charAt(0).toUpperCase() + criteria1.slice(1);
   return arr.filter(function(obj) {
     return Object.keys(criteria).every(function(c) {
       debugger
